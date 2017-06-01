a8f1fbe061859a4740f818875f09ae2dc21a5f33
zateyev <zhasulan.ateyev@gmail.com>
2017-06-01 17:23:30 +0200

    separating shader loading

--- common/shader.hpp
+++ common/shader.hpp
@@ -1,6 +1,10 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
-GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+GLboolean compileCheck(GLuint shader);
+GLuint initShaderObj(const GLchar* srcfile, GLenum shaderType);
+GLuint createShaderPgm();
+void linkShader(GLuint shaderPgm, GLuint newVertHandle, GLuint newFragHandle);
+GLint checkShaderLinkStatus(GLuint pgmHandle);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -11,11 +11,12 @@
 #include <include/GL/glm/gtc/type_ptr.hpp>
 
 #include <common/texture.hpp>
+#include <common/shader.hpp>
 
 #include <include/GL/glui.h>
 
 #include <iomanip> // setprecision
-#include <sstream> // stringstream
+#include <sstream>
 
 
 #define GL_ERROR() checkForOpenGLError(__FILE__, __LINE__)
@@ -79,7 +80,7 @@ float g_OpacityVal = 40.0; // 40
 float g_ColorVal = 1.0; // 0.4
 float g_AbsorptionModeIndex = 1.0; // -1.0 ? 1
 
-bool loadPngImage(const char *name, int &outWidth, int &outHeight, GLubyte **outData);
+// bool loadPngImage(const char *name, int &outWidth, int &outHeight, GLubyte **outData);
 
 int tr_width = 256;
 int tr_height = 10;
@@ -242,93 +243,6 @@ void drawBox(GLenum glFaces)
     glDisable(GL_CULL_FACE);
 }
 
-// check the compilation result
-GLboolean compileCheck(GLuint shader)
-{
-    GLint err;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &err);
-    if (GL_FALSE == err)
-    {
-    	GLint logLen;
-    	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLen);
-    	if (logLen > 0)
-    	{
-  	    char* log = (char *)malloc(logLen);
-  	    GLsizei written;
-  	    glGetShaderInfoLog(shader, logLen, &written, log);
-  	    cerr << "Shader log: " << log << endl;
-  	    free(log);
-    	}
-    }
-    return err;
-}
-
-// init shader object
-GLuint initShaderObj(const GLchar* srcfile, GLenum shaderType)
-{
-    ifstream inFile(srcfile, ifstream::in);
-    // use assert?
-    if (!inFile)
-    {
-    	cerr << "Error openning file: " << srcfile << endl;
-    	exit(EXIT_FAILURE);
-    }
-
-    const int MAX_CNT = 10000;
-    GLchar *shaderCode = (GLchar *) calloc(MAX_CNT, sizeof(GLchar));
-    inFile.read(shaderCode, MAX_CNT);
-    if (inFile.eof())
-    {
-    	size_t bytecnt = inFile.gcount();
-    	*(shaderCode + bytecnt) = '\0';
-    }
-    else if(inFile.fail()) cout << srcfile << "read failed \n";
-    else cout << srcfile << "is too large\n";
-    // create the shader Object
-    GLuint shader = glCreateShader(shaderType);
-    if (0 == shader) cerr << "Error creating vertex shader.\n";
-    const GLchar* codeArray[] = {shaderCode};
-    glShaderSource(shader, 1, codeArray, NULL);
-    free(shaderCode);
-
-    // compile the shader
-    glCompileShader(shader);
-    if (GL_FALSE == compileCheck(shader)) cerr << "shader compilation failed\n";
-    return shader;
-}
-
-GLint checkShaderLinkStatus(GLuint pgmHandle)
-{
-    GLint status;
-    glGetProgramiv(pgmHandle, GL_LINK_STATUS, &status);
-    if (GL_FALSE == status)
-    {
-    	GLint logLen;
-    	glGetProgramiv(pgmHandle, GL_INFO_LOG_LENGTH, &logLen);
-    	if (logLen > 0)
-    	{
-    	    GLchar * log = (GLchar *)malloc(logLen);
-    	    GLsizei written;
-    	    glGetProgramInfoLog(pgmHandle, logLen, &written, log);
-    	    cerr << "Program log: " << log << endl;
-    	}
-    }
-    return status;
-}
-
-// link shader program
-GLuint createShaderPgm()
-{
-    // Create the shader program
-    GLuint programHandle = glCreateProgram();
-    if (0 == programHandle)
-    {
-    	cerr << "Error create shader program" << endl;
-    	exit(EXIT_FAILURE);
-    }
-    return programHandle;
-}
-
 GLuint initFace2DTex(GLuint bfTexWidth, GLuint bfTexHeight)
 {
     GLuint backFace2DTex;
@@ -457,35 +371,6 @@ void initShader()
     g_rcFragHandle = initShaderObj("../shader/secondPass.frag", GL_FRAGMENT_SHADER);
 // create the shader program , use it in an appropriate time
     g_programHandle = createShaderPgm();
-}
-
-// link the shader objects using the shader program
-void linkShader(GLuint shaderPgm, GLuint newVertHandle, GLuint newFragHandle)
-{
-    const GLsizei maxCount = 2;
-    GLsizei count;
-    GLuint shaders[maxCount];
-    glGetAttachedShaders(shaderPgm, maxCount, &count, shaders);
-
-    GL_ERROR();
-    for (int i = 0; i < count; i++) {
-	     glDetachShader(shaderPgm, shaders[i]);
-    }
-    // Bind index 0 to the shader input variable "VerPos"
-    glBindAttribLocation(shaderPgm, 0, "position");
-    // Bind index 1 to the shader input variable "VerClr"
-    glBindAttribLocation(shaderPgm, 1, "vertColor");
-    GL_ERROR();
-    glAttachShader(shaderPgm,newVertHandle);
-    glAttachShader(shaderPgm,newFragHandle);
-    GL_ERROR();
-    glLinkProgram(shaderPgm);
-    if (GL_FALSE == checkShaderLinkStatus(shaderPgm))
-    {
-    	cerr << "Failed to relink shader program!" << endl;
-    	exit(EXIT_FAILURE);
-    }
-    GL_ERROR();
 }
 
 void display()
